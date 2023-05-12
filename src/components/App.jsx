c417b2d960a70548aa577a0e08da63664da6d966
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-05-12 10:13:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,6 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsFilter } from './ContactsFilter/ContactsFilter';
 import { ContactsList } from './ContactsList/ContactsList';
 
-const testContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 const LS_KEY = 'phoebook_contact';
 
 export class App extends Component {
